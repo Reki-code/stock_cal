6f21d3af5696d3d4bd591a92bbd530be77e3ca0a
Yuuta <darkmasterwei@gmail.com>
2026-02-01 13:01:05 +0800

    Add report generator for annual income reports

---
+++ src/report_generator.py
@@ -0,0 +1,11 @@
+import datetime
+
+
+def generate_annual_income_report(year):
+    # Placeholder for generating annual income report
+    report_date = datetime.datetime(year, 1, 1)
+    print(f"Generating annual income report for {year} on {report_date}")
+
+if __name__ == '__main__':
+    year_to_generate = 2026  # Change this to generate report for a different year
+    generate_annual_income_report(year_to_generate)
